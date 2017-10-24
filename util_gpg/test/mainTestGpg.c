6cb24de0d86a664b931d95a56f3ab7124d7942ae
Mauro Mombelli <mauro@mombelli.me>
2017-10-25 01:13:36 +0200

    added test

--- util_gpg/test/mainTestGpg.c
+++ util_gpg/test/mainTestGpg.c
@@ -8,7 +8,7 @@
 
 int main(int argc, const char *argv[]){
 	gpgme_ctx_t ctx;
-	gpgsession_new(&ctx, true);
+	gpgsession_new(&ctx, false);
 	
 	int inkeysfd = -1;
 	if (argc > 1) {
@@ -19,27 +19,26 @@ int main(int argc, const char *argv[]){
 	}
 	
 	if (inkeysfd != -1) {
-		gpgme_error_t gerr;
+		gpgme_error_t err;
 		gpgme_data_t d = NULL;
-		if ((gerr = gpgme_data_new_from_fd(&d, inkeysfd))) {
-			fprintf(stderr, "failed to initialize new gpgme data. (%d) %s\n", gerr, gpgme_strerror(gerr));
+		err = gpgme_data_new_from_fd(&d, inkeysfd);
+		if (gpgme_err_code(err) != GPG_ERR_NO_ERROR) {
+			fprintf(stderr, "failed to initialize new gpgme data. (%d) %s\n", err, gpgme_strerror(err));
 			return -1;
 		}
 		printf("ok stream\n");
 		/* FIXME: blocking */
-		gerr = gpgme_op_import(ctx, d);
-		printf("ok import\n");
+		err = gpgme_op_import(ctx, d);
+		printf("done import\n");
 		gpgme_data_release(d);
-		if (gerr) {
-			fprintf(stderr, "Failed to import key(s) from file '%s', falling back to normal GnuPG: (%d) %s\n", argv[1], gerr, gpgme_strerror(gerr));
+		if (gpgme_err_code(err) != GPG_ERR_NO_ERROR){
+			fprintf(stderr, "Failed to import key(s) from file '%s', falling back to normal GnuPG: (%d) %s\n", argv[1], err, gpgme_strerror(err));
 			//gpgsession_free(skt->fromfile, skt->log_level);
 		}/* else if (gpgsession_gather_secret_keys(skt->fromfile)) {
 			fprintf(stderr, "Failed to learn the secret key(s) available in %s\n", argv[1]);
 			//gpgsession_free(skt->fromfile, skt->log_level);
 		}*/
 	}
-	
-	
 	
 	gpgsession_gather_secret_keys(&ctx);
 }
---
+++ util_gpg/test/run-test.sh
@@ -0,0 +1,16 @@
+ 
+#!/bin/bash
+
+make
+
+export GNUPGHOME=/tmp/tmpKey
+
+if [ ! -d $GNUPGHOME ] 
+then
+    mkdir -p $GNUPGHOME
+    gpg --batch --pinentry-mode=loopback --passphrase-fd=4 4<<<'' --quick-gen-key 'test user <test@example.org>'
+fi
+
+./testGpg
+
+gpgconf --kill all
